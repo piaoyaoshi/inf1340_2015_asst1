7c1cdcbd945179a487f1d52da06158e5b0e24ab1
piaoyaoshi <piaoyao.shi@mail.utoronto.ca>
2015-10-01 10:37:28 -0400

    testcase_exe1

--- exercise1.py
+++ exercise1.py
@@ -13,5 +13,23 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
-money = 1000.00
-print(money)
+money = 0
+commission_rate = 0.03
+
+qty_bought = 2000
+purchase_price = 900
+
+qty_sold = 2000
+sell_price = 942.73
+
+book_value = qty_bought * purchase_price
+amount_received = qty_sold * sell_price
+
+money = money + amount_received * (1 - commission_rate) - book_value * commission_rate
+profit = amount_received - book_value - (amount_received + book_value) * commission_rate
+
+print('Lakshmi had $'),
+print(money),
+print('left, and she made a profit of $'),
+print('%.2f' % profit),
+print('(negative sign means a loss).')
