5be208995e2c84ce1793ca3936fe366768109d4c
landongr <graham.cilandon@gmail.com>
2015-10-08 11:26:51 -0400

    Attempted Bug Fix

--- exercise3.py
+++ exercise3.py
@@ -30,9 +30,9 @@ def diagnose_car():
 
     def check_silence():
         silent = raw_input("Is the car silent when you turn the key?")
-        if silent == "y":
+        if silent == "Y":
             check_battery()
-        elif silent == "n":
+        elif silent == "N":
             check_noise()
         else:
             print (error)
@@ -41,11 +41,11 @@ def diagnose_car():
     # This is the left side of the flowchart
     def check_battery():
         corroded = raw_input("Are the battery terminals corroded?")
-        if corroded == "y":
+        if corroded == "Y":
             print ("Clean terminals and try starting again.")
             exit()
-        elif corroded == "n":
-            print ("Replace cables and try again")
+        elif corroded == "N":
+            print ("Replace cables and try again.")
             exit()
         else:
             print (error)
@@ -54,10 +54,10 @@ def diagnose_car():
     # Everything below is the right side of the flow chart
     def check_noise():
         click = raw_input("Does the car make a clicking noise?")
-        if click == "y":
-            print ("Replace the battery")
+        if click == "Y":
+            print ("Replace the battery.")
             exit()
-        elif click == "n":
+        elif click == "N":
             check_crank()
         else:
             print (error)
@@ -65,10 +65,10 @@ def diagnose_car():
 
     def check_crank():
         crank = raw_input("Does the car crank up but fail to start?")
-        if crank == "y":
+        if crank == "Y":
             print ("Check spark plug connections.")
             exit()
-        elif crank == "n":
+        elif crank == "N":
             check_engine()
         else:
             print (error)
@@ -76,10 +76,10 @@ def diagnose_car():
 
     def check_engine():
         engine = raw_input("Does engine start and then die?")
-        if engine == "y":
+        if engine == "Y":
             check_fuel()
-        elif engine == "n":
-            print ("Engine is not getting enough fuel. Clean fuel pump")
+        elif engine == "N":
+            print ("Engine is not getting enough fuel. Clean fuel pump.")
             exit()
         else:
             print (error)
@@ -87,10 +87,10 @@ def diagnose_car():
 
     def check_fuel():
         fuel = raw_input("Does your car have fuel injection?")
-        if fuel == "n":
-            print ("Check to ensure the choke is opening and closing")
+        if fuel == "N":
+            print ("Check to ensure the choke is opening and closing.")
             exit()
-        elif fuel == "y":
+        elif fuel == "Y":
             print ("Get it in for service")
             exit()
         else:
