c8b4694566bce0fb07667c40a2c1d0fa42c73c20
piaoyaoshi <piaoyao.shi@mail.utoronto.ca>
2015-10-08 21:53:50 -0400

    exe1_10.08.1_good_copy

--- exercise1.py
+++ exercise1.py
@@ -5,6 +5,11 @@
 This module prints the amount of money that Lakshmi has remaining
 after the stock transactions
 
+Lakshmi bought some shares sometime ago. She later sold them.
+On both occasions, she had to pay the stockbroker for his/her services.
+Now, she is having troubles calculating how much she made or lost during the transaction.
+This program fixes that! Now, she's soooo happy :)
+But,the result shows that she has a loss of $ 25065.00.Now, she is not happy any more :(
 """
 
 __author__ = 'Susan Sim'
@@ -12,16 +17,7 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-""" Lakshmi bought some shares sometime ago. She later sold them.
-    On both occasions, she had to pay the stockbroker for his/her services.
-
-    Now, she is having troubles calculating how much she made or lost during the transaction.
-    This program fixes that! Now, she's soooo happy :)
-
-    But,the result shows that she has a loss of $ 25065.00.Now, she is not happy any more :(
-"""
-
-# valuables needed for this programming
+# valuables needed for calculating profit
 commission_rate = 0.03
 stock_quantity = 2000
 cost_price = 900
@@ -39,5 +35,5 @@ amount_received = stock_income - selling_commission
 
 profit = amount_received - amount_spent
 
-print('Now she has a profit of %.2f' % profit),
-print('(negative sign means a loss).')
+print("Now she has a profit of %.2f" % profit),
+print("(negative sign means a loss).")
